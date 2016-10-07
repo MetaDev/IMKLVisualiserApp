6824a147d0d6fa8c244076d2aa4edadbdd61ddda
Harald De Bondt <haralddebondt@gmail.com>
2016-10-07 12:25:34 +0200

    t

--- Assets/Scripts/UI/GUIFactory.cs
+++ Assets/Scripts/UI/GUIFactory.cs
@@ -15,7 +15,6 @@ using Utility;
 
 public class GUIFactory : MonoBehaviour
 {
-    public MenuPanel menuPanel;
     public CustomToggle MenuToggle;
     public MultiSelectPanel IMKLPackageInfoPanel;
     public MultiSelectPanel IMKLPackageDrawPanel;
@@ -26,6 +25,8 @@ public class GUIFactory : MonoBehaviour
     public Button HiddenMenuBtn;
     public GameObject HiddenMenu;
     public Toggle GPSToggle;
+    public InputField AuthCodeInputField;
+    public Button OKLogin;
 
 
     public void LoadPacketInfo()
@@ -132,7 +133,7 @@ public class GUIFactory : MonoBehaviour
 
     public void LoginPress()
     {
-        var authCode = menuPanel.AuthCodeInputField.text;
+        var authCode = AuthCodeInputField.text;
         WebService.LoginWithAuthCode(authCode).DoOnError(error => GUIFactory.instance.MyModalWindow.Show(error.Message, true))
             .Subscribe(webRequest => GUIFactory.instance.MyModalWindow.Show("Login succeeded", true));
     }
